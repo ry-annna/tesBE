7421ebed5a66b65a4f509058a8f7df2cbb9768b9
SuperNayr135 <ryanadytia219@gmail.com>
2023-08-29 22:44:08 +0700

    adad

--- api/config/database.js
+++ api/config/database.js
@@ -4,7 +4,7 @@ const dbPool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: "u3799030_express",
 });
 
 module.exports = dbPool.promise();
