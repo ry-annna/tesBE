e6894b916fef29f04afadac067bc963046b1195c
SuperNayr135 <ryanadytia219@gmail.com>
2023-09-01 11:19:48 +0700

    adada

--- api/index.js
+++ api/index.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-app.use(cors());
-
 const PORT = process.env.PORT || 4000;
 
 const usersRoutes = require("./routes/akun-demo.js");
@@ -11,6 +9,7 @@ const beritaRoutes = require("./routes/berita.js");
 
 const app = express();
 
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
